32a1359aa361e2c82abf771834f0a1dca554c747
Sylvester Aswin Stanley <sylvester.aswin.stanley@zalando.de>
2020-02-24 21:32:16 +0530

    make yarn json compatible with > 22 version

--- src/dependency-graph.ts
+++ src/dependency-graph.ts
@@ -25,17 +25,14 @@ interface AtomsInfo {
 }
 
 const buildAtomGraph = (atoms: AtomsInfo): AtomGraph => {
-  return Object.keys(atoms).reduce(
-    (forest, name) => {
-      const { path, children, parents, pkg, isCss } = atoms[name];
-      forest[name] = new Atom({ name, path, atoms: forest, isCss })
-        .withParents(parents)
-        .withChildren(children)
-        .withPackage(pkg);
-      return forest;
-    },
-    {} as AtomGraph
-  );
+  return Object.keys(atoms).reduce((forest, name) => {
+    const { path, children, parents, pkg, isCss } = atoms[name];
+    forest[name] = new Atom({ name, path, atoms: forest, isCss })
+      .withParents(parents)
+      .withChildren(children)
+      .withPackage(pkg);
+    return forest;
+  }, {} as AtomGraph);
 };
 
 export interface AtomGraph {
@@ -46,27 +43,25 @@ const generateDependencyGraph = async (
   atomsPathRE: RegExp
 ): Promise<AtomGraph> => {
   const infoString = await run("yarn workspaces info --json");
-  const data = JSON.parse(infoString).data;
+  const infoStringJSON = JSON.parse(infoString);
+  const data = infoStringJSON.data ? infoStringJSON.data : infoStringJSON;
   const info: YarnInfo = JSON.parse(data);
-  const atomsInfo = Object.keys(info).reduce(
-    (prev, next) => {
-      if (atomsPathRE.test(info[next].location)) {
-        const { workspaceDependencies, location } = info[next];
-        const pkg = require(path.join(process.cwd(), location, "package.json"));
-        const indexCssPath = path.join(process.cwd(), location, "index.css");
-        const isCss = fs.existsSync(indexCssPath);
-        prev[next] = {
-          path: location,
-          children: [],
-          parents: workspaceDependencies,
-          pkg,
-          isCss
-        };
-      }
-      return prev;
-    },
-    {} as AtomsInfo
-  );
+  const atomsInfo = Object.keys(info).reduce((prev, next) => {
+    if (atomsPathRE.test(info[next].location)) {
+      const { workspaceDependencies, location } = info[next];
+      const pkg = require(path.join(process.cwd(), location, "package.json"));
+      const indexCssPath = path.join(process.cwd(), location, "index.css");
+      const isCss = fs.existsSync(indexCssPath);
+      prev[next] = {
+        path: location,
+        children: [],
+        parents: workspaceDependencies,
+        pkg,
+        isCss
+      };
+    }
+    return prev;
+  }, {} as AtomsInfo);
 
   const atoms = Object.keys(atomsInfo).reduce((prev, next) => {
     const dependencies = atomsInfo[next].parents;
